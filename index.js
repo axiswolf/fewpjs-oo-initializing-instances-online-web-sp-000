d9bf0f46a4de3441f155be1bb43c07cfd1e1eb5c
Karen V <karenvo@usc.edu>
2020-01-08 13:33:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ class Breakfast {
     this.drink = drink;
   }
 }
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, _dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = _dessert;
+  }
+}
